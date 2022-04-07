630db9e328453efe4e75e18f953699d0b9887c6e
Velrosa <steezie21@hotmail.co.uk>
2022-04-07 22:30:16 +0100

    Add project files.

---
+++ Phonebook/Migrations/20220407212426_CreateContactsDB.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Phonebook.Migrations
+{
+    public partial class CreateContactsDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Phonenumber = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contacts", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contacts");
+        }
+    }
+}
---
+++ Phonebook/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Configuration;
+
+namespace Phonebook
+{
+    internal class Program
+    {
+        string dbString = ConfigurationManager.AppSettings.Get("dbString");
+        static void Main(string[] args)
+        {
+            using (var context = new ContactContext())
+            {
+                var con = new Contact()
+                {
+                    Id = 1,
+                    Name = "Bill",
+                    Phonenumber = "03030300000"                    
+                };
+            }
+        }
+
+        public class ContactContext : DbContext
+        {
+            public DbSet<Contact> Contacts { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(LocalDb)\LocalDBFlashcards;Database=ContactsDB;Integrated Security=True");
+            }
+        }
+    }
+}
